074ef3f2ce6c074b0a10961d1b64c4029e1ab3d1
Jake Mitchell <j.mitchell7574@gmail.com>
2025-02-05 10:01:14 +0000

    Began working on Feedback Message implementation

---
+++ FeedTrac.Server/Database/FeedbackMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FeedTrac.Server.Database
+{
+    public class FeedbackMessage
+    {
+        [Required]
+        [Key]
+        public int MessageId { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public required string Content { get; set; }
+
+        [Required]
+        public required FeedbackTicket Ticket { get; set; }
+
+        [Required]
+        public required ApplicationUser Author { get; set; }
+
+        [Required]
+        public required DateTime CreatedAt { get; set; }
+    }
+}
