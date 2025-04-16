92f9ccab16dbdb2ee94f915b77852dca5a85e17d
Jake Mitchell <j.mitchell7574@gmail.com>
2025-04-16 21:27:30 +0100

    Added more Dto's for identity

---
+++ FeedTrac.Server/Models/Responses/Identity/BaseUserInfo.cs
@@ -0,0 +1,29 @@
+﻿using FeedTrac.Server.Database;
+
+namespace FeedTrac.Server.Models.Responses.Identity
+{
+    /// <summary>
+    /// Data structure containing basic user information.
+    /// Prevents sending sensitive information to the client.
+    /// </summary>
+    public class BaseUserInfo
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Constructor for BaseUserInfo
+        /// </summary>
+        /// <param name="au">The application user to build the info from</param>
+        public BaseUserInfo(ApplicationUser au)
+        {
+            UserId = au.Id;
+            FirstName = au.FirstName ?? string.Empty;
+            LastName = au.LastName ?? string.Empty;
+            Email = au.Email ?? string.Empty;
+        }
+
+    }
+}
