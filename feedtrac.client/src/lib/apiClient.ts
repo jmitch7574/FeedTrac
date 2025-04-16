098f5231ea7b3d6bea737467277364a39d14baf4
umarnauman <umarnauman01@gmail.com>
2025-04-16 07:02:41 +0100

    removed a comment within axios api

--- feedtrac.client/src/lib/apiClient.ts
+++ feedtrac.client/src/lib/apiClient.ts
@@ -3,7 +3,7 @@ const API_BASE_URL = import.meta.env.VITE_FEEDTRAC_ENV_KEY;
 
 export const apiClient = axios.create({
   baseURL: `${API_BASE_URL}`,
-  withCredentials: true, // ⬅️ Important for cookies/session auth
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
