84815dccfc5fbe19278953fa0f7bd8cf6056d8a4
Jake Mitchell <j.mitchell7574@gmail.com>
2025-05-07 20:01:35 +0100

    Removed funny numbers from clipboard button

--- feedtrac.client/src/components/modules/CopyToClipboard.tsx
+++ feedtrac.client/src/components/modules/CopyToClipboard.tsx
@@ -1,18 +1,16 @@
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
+import React from "react";
 import { Copy } from "lucide-react";
 
 const CopyToClipboard = ({ text }: { text: string }) => {
-  const [copied, setCopied] = useState(false);
 
   const handleCopy = async (e: React.MouseEvent) => {
     e.stopPropagation();
     e.preventDefault();
     try {
       await navigator.clipboard.writeText(text);
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000); // Reset after 2s
+      toast.success("Copied to clipboard!")
     } catch (err) {
       console.error("Failed to copy: ", err);
     }
@@ -21,13 +19,6 @@ const CopyToClipboard = ({ text }: { text: string }) => {
   return (
     <Button variant='outline' size='icon' onClick={handleCopy}>
       <Copy className='h-4 w-4' />
-      <span className='sr-only'>Copy</span>
-      {copied && (
-        <>
-          {toast.success("Copied to clipboard!")}
-          {/* <span className='ml-2 text-sm text-green-500'>Copied!</span> */}
-        </>
-      )}
     </Button>
   );
 };
