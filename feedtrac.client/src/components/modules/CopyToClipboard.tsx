78a6c5e624dcb0432a4ad81e757ef979d364452b
adriann1z <162132160+adriann1z@users.noreply.github.com>
2025-05-07 01:34:44 +0100

    Created a copy clipboard component.

---
+++ feedtrac.client/src/components/modules/CopyToClipboard.tsx
@@ -0,0 +1,32 @@
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Copy } from "lucide-react";
+
+
+const CopyToClipboard = ({text} : {text: string}) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(text);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000); // Reset after 2s
+          } catch (err) {
+            console.error("Failed to copy: ", err);
+          }
+    }
+
+    return (
+        <Button variant="outline" size="icon" onClick={handleCopy}>
+          <Copy className="h-4 w-4" />
+          <span className="sr-only">Copy</span>
+          {copied && (
+            <span className="ml-2 text-sm text-green-500">Copied!</span>
+          )}
+        </Button>
+      );
+    
+}
+
+export default CopyToClipboard
